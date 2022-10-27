63c5d804382dd786cea65c7c9a75c908540818cc
서정희 <sjhlko@naver.com>
2022-10-28 02:21:30 +0900

    hello api 추가

---
+++ src/main/java/com/springboot/homework/controller/GetController.java
@@ -0,0 +1,14 @@
+package com.springboot.homework.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello(){
+        return "hello";
+    }
+}
